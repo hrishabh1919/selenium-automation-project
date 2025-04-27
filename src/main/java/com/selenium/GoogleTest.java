2dd6d3741303b4a0ebf3d9e3f096f0683b620604
hrishabh1919 <hrishiofficial01@gmail.com>
2025-04-27 22:36:00 +0530

    Initial commit of Selenium Automation Project

---
+++ src/main/java/com/selenium/GoogleTest.java
@@ -0,0 +1,25 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Page title is: " + driver.getTitle());
+
+        try {
+            
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+    }
+}
