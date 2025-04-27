d014166311ed8d228567c3793dde84279dd9cb86
hrishabh1919 <hrishiofficial01@gmail.com>
2025-04-28 00:36:54 +0530

    Testing Jenkins trigger

--- src/main/java/com/selenium/GoogleTest.java
+++ src/main/java/com/selenium/GoogleTest.java
@@ -1,30 +1,45 @@
 package com.selenium;
 
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTest {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+        // Setup ExtentReports
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+        ExtentTest test = extent.createTest("Open Google Test", "Opening Google website and verifying title");
 
+        // Setup WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com");
-
-        System.out.println("Page title is: " + driver.getTitle());
-
-
-        // test statement for jenkins 
-        System.out.println("Test Triggered !"); 
-
-
         try {
-            
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+            // Test Steps
+            driver.get("https://www.google.com");
+            String title = driver.getTitle();
+            System.out.println("Page Title: " + title);
 
-        driver.quit();
+            // Logging to report
+            if (title.contains("Google")) {
+                test.pass("Google website opened successfully with title: " + title);
+            } else {
+                test.fail("Google website did not open properly. Title was: " + title);
+            }
+
+        } catch (Exception e) {
+            test.fail("Test failed with exception: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Close Browser
+            driver.quit();
+            System.out.println("Test Completed. Browser closed.");
+
+            // Finalize ExtentReport
+            extent.flush();
+        }
     }
 }
